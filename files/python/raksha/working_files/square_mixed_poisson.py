09b89557df981f5dc5e2b2cf78db17f1f3b12334
Raksha Konanur <raksha.konanur@berkeley.edu>
2025-05-06 11:59:28 -0700

    Adding tutorial for mixed formulation of poisson: could be useful for darcy solve

---
+++ files/python/raksha/working_files/square_mixed_poisson.py
@@ -0,0 +1,89 @@
+from mpi4py import MPI
+from petsc4py import PETSc
+
+import numpy as np
+
+from basix.ufl import element, mixed_element
+from dolfinx import fem, io, mesh
+from dolfinx.fem.petsc import LinearProblem
+from ufl import (Measure, SpatialCoordinate, TestFunctions, TrialFunctions,
+                 div, exp, inner)
+
+"""
+    From the DOLFINx tutorial: Mixed formulation of the Poisson equation
+    https://docs.fenicsproject.org/dolfinx/v0.7.2/python/demos/demo_mixed-poisson.html
+"""
+
+
+
+
+domain = mesh.create_unit_square(MPI.COMM_WORLD, 32, 32, mesh.CellType.quadrilateral)
+
+k = 1
+Q_el = element("BDMCF", domain.basix_cell(), k)
+P_el = element("DG", domain.basix_cell(), k - 1)
+V_el = mixed_element([Q_el, P_el])
+V = fem.functionspace(domain, V_el)
+
+(sigma, u) = TrialFunctions(V)
+(tau, v) = TestFunctions(V)
+
+x = SpatialCoordinate(domain)
+f = 10.0 * exp(-((x[0] - 0.5) * (x[0] - 0.5) + (x[1] - 0.5) * (x[1] - 0.5)) / 0.02)
+
+dx = Measure("dx", domain)
+a = inner(sigma, tau) * dx + inner(u, div(tau)) * dx + inner(div(sigma), v) * dx
+L = -inner(f, v) * dx
+
+
+fdim = domain.topology.dim - 1
+facets_top = mesh.locate_entities_boundary(domain, fdim, lambda x: np.isclose(x[1], 1.0))
+Q, _ = V.sub(0).collapse()
+dofs_top = fem.locate_dofs_topological((V.sub(0), Q), fdim, facets_top)
+
+
+def f1(x):
+    values = np.zeros((2, x.shape[1]))
+    values[1, :] = np.sin(5 * x[0])
+    return values
+
+
+f_h1 = fem.Function(Q)
+f_h1.interpolate(f1)
+bc_top = fem.dirichletbc(f_h1, dofs_top, V.sub(0))
+
+
+facets_bottom = mesh.locate_entities_boundary(domain, fdim, lambda x: np.isclose(x[1], 0.0))
+dofs_bottom = fem.locate_dofs_topological((V.sub(0), Q), fdim, facets_bottom)
+
+
+def f2(x):
+    values = np.zeros((2, x.shape[1]))
+    values[1, :] = -np.sin(5 * x[0])
+    return values
+
+
+f_h2 = fem.Function(Q)
+f_h2.interpolate(f2)
+bc_bottom = fem.dirichletbc(f_h2, dofs_bottom, V.sub(0))
+
+
+bcs = [bc_top, bc_bottom]
+
+problem = LinearProblem(a, L, bcs=bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu",
+                                                      "pc_factor_mat_solver_type": "mumps"})
+try:
+    w_h = problem.solve()
+except PETSc.Error as e:  # type: ignore
+    if e.ierr == 92:
+        print("The required PETSc solver/preconditioner is not available. Exiting.")
+        print(e)
+        exit(0)
+    else:
+        raise e
+
+sigma_h, u_h = w_h.split()
+
+with io.XDMFFile(domain.comm, "out_mixed_poisson/u.xdmf", "w") as file:
+    file.write_mesh(domain)
+    file.write_function(u_h)

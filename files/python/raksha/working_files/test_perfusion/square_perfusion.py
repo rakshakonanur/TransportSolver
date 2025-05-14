990c901b6958fa13aeeca9419f36c56b9078f4f9
Raksha Konanur <raksha.konanur@berkeley.edu>
2025-05-14 15:18:02 -0700

    Complete overhaul of old code: pressure working, velocity need to be updated

--- files/python/raksha/working_files/test_perfusion/square_perfusion.py
+++ files/python/raksha/working_files/test_perfusion/square_perfusion.py
@@ -1,6 +1,7 @@
 import ufl
 import numpy   as np
 import dolfinx as dfx
+import matplotlib.pyplot as plt
 
 from ufl               import avg, jump, dot, grad
 from sys               import argv
@@ -9,13 +10,17 @@ from pathlib           import Path
 from petsc4py          import PETSc
 from basix.ufl         import element, mixed_element
 from dolfinx import (fem, io, mesh)
-from dolfinx.fem.petsc import assemble_matrix, create_vector, assemble_vector, set_bc, apply_lifting, LinearProblem
+from dolfinx.fem.petsc import assemble_matrix, create_vector, assemble_vector, set_bc, apply_lifting, LinearProblem, set_bc
 from ufl import (FacetNormal, Identity, Measure, TestFunctions, TrialFunctions, exp, div, inner, SpatialCoordinate,
                  as_vector, div, dot, ds, dx, inner, lhs, grad, nabla_grad, rhs, sym, system)
+from typing import List, Optional
 
 """
     From the DOLFINx tutorial: Mixed formulation of the Poisson equation
     https://docs.fenicsproject.org/dolfinx/v0.7.2/python/demos/demo_mixed-poisson.html
+
+    More tutorials: https://github.com/Simula-SSCP/SSCP_lectures/
+
 """
 
 print = PETSc.Sys.Print
@@ -87,86 +92,62 @@ class PerfusionSolver:
         self.H = H
         self.L = L
         self.N = N_cells
-        self.mesh, self.ft = create_rectangle_mesh_with_tags(H, L, N_cells=N_cells)
+        self.mesh, self.ft = create_rectangle_mesh_with_tags(L, H, N_cells=N_cells)
         self.D_value = D_value
         self.element_degree = element_degree
         self.write_output = write_output
 
     def setup(self):
         ''' Setup the solver. '''
-        fdim = self.mesh.topology.dim - 1  
-        k = self.element_degree
-        Q_el = element("BDMCF", self.mesh.basix_cell(), k)
-        P_el = element("DG", self.mesh.basix_cell(), k - 1)
-        V_el = mixed_element([Q_el, P_el])
-        V = fem.functionspace(self.mesh, V_el)
-        beta  = dfx.fem.Constant(self.mesh, dfx.default_scalar_type(100.0)) # Nitsche penalty parameter
-        hf = ufl.CellDiameter(self.mesh) # Cell diameter
-
-        (sigma, u) = TrialFunctions(V)
-        (tau, v) = TestFunctions(V)
-
-        x = SpatialCoordinate(self.mesh)
-        # f = 0.0 * exp(-((x[0] - self.L/2) * (x[0] - self.L/2) + (x[1] - self.H/2) * (x[1] - self.H/2)) / 0.05)
-        f = dfx.fem.Constant(self.mesh, dfx.default_scalar_type(0.0)) # Source term 
-
-        # facets_top = mesh.locate_entities_boundary(self.mesh, fdim, TOP)
-        Q, _ = V.sub(0).collapse()
-        P,_ = V.sub(1).collapse()
-        dofs_top = fem.locate_dofs_topological((V.sub(0), Q), fdim, self.ft.find(TOP))
-
-        # Dirichlet BC for the right side of the square
-        self.bc_func_right = dfx.fem.Function(P)
-        self.bc_func_right.interpolate(lambda x: np.full(x.shape[1], 0.0))
-
-        self.bc_func_left = dfx.fem.Function(P)
-        self.bc_func_left.interpolate(lambda x: np.full(x.shape[1], 100.0))
-
-        self.ds = ufl.Measure('ds', domain=self.mesh, subdomain_data=self.ft) # Exterior facet integrals
+        
+        fdim = self.mesh.topology.dim -1 
+        # k = self.element_degree
+        k = 1
+        P_el = element("Lagrange", self.mesh.basix_cell(), k)
+        u_el = element("DG", self.mesh.basix_cell(), k-1)
         dx = Measure("dx", self.mesh)
-        a = inner(sigma, tau) * dx + inner(u, div(tau)) * dx + inner(div(sigma), v) * dx
-        # Weakly enforced BC on the right-hand side of the square, using Nitsche's method
-        a4 = beta / hf * u * v * self.ds(RIGHT)
-        a4 += beta / hf * u * v * self.ds(LEFT)
-        a += a4
 
-        L = -inner(f, v) * dx
-        L += beta / hf * self.bc_func_right* v * self.ds(RIGHT) 
-        L += beta / hf * self.bc_func_left* v * self.ds(LEFT) # Weakly enforce Dirichlet BC using Nitsche's method
+        # Define function spaces
+        W = dfx.fem.functionspace(self.mesh, P_el) # Pressure function space
+        V = dfx.fem.functionspace(self.mesh, u_el) # Velocity function space
 
-        # def f1(x):
-        #     values = np.zeros((2, x.shape[1]))
-        #     values[1, :] = np.sin(5 * x[0])
-        #     return values
+        kappa = 1
+        mu = 1
+        kappa_over_mu = fem.Constant(self.mesh, dfx.default_scalar_type(kappa/mu))
+        phi = fem.Constant(self.mesh, dfx.default_scalar_type(0.1)) # Porosity of the medium, ranging from 0 to 1
+        S = fem.Constant(self.mesh, dfx.default_scalar_type(0.0)) # Source term
 
+        # Trial and test functions
+        v, p = ufl.TestFunction(W), ufl.TrialFunction(W)
 
-        # f_h1 = fem.Function(Q)
-        # f_h1.interpolate(f1)
-        # bc_top = fem.dirichletbc(f_h1, dofs_top, V.sub(0))
+        # Boundary conditions
+        bc_left  = dfx.fem.Constant(self.mesh, dfx.default_scalar_type(1.0))
+        bc_right = dfx.fem.Constant(self.mesh, dfx.default_scalar_type(0.0))
+        dof_left  = dfx.fem.locate_dofs_geometrical(W, lambda x: np.isclose(x[0], 0.0))
+        dof_right = dfx.fem.locate_dofs_geometrical(W, lambda x: np.isclose(x[0], L))
+        bcs = [dfx.fem.dirichletbc(bc_left, dof_left, W),
+                dfx.fem.dirichletbc(bc_right, dof_right, W)
+        ]
 
+        # Define variational problem
+        a = kappa_over_mu * dot(grad(p), grad(v)) * ufl.dx 
+        f = S * v * ufl.dx  # residual form of our equation
 
-        # # facets_bottom = mesh.locate_entities_boundary(self.mesh, fdim, BOT)
-        # dofs_bottom = fem.locate_dofs_topological((V.sub(0), Q), fdim, self.ft.find(BOT))
+        self.a = a
+        self.f = f
 
+        # Apply Dirichlet BCs
+        self.bcs = bcs
 
-        # def f2(x):
-        #     values = np.zeros((2, x.shape[1]))
-        #     values[1, :] = -np.sin(5 * x[0])
-        #     return values
+        problem = LinearProblem(self.a, self.f, bcs=self.bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"})
 
-
-        # f_h2 = fem.Function(Q)
-        # f_h2.interpolate(f2)
-        # bc_bottom = fem.dirichletbc(f_h2, dofs_bottom, V.sub(0))
-
-
-        # bcs = [bc_top, bc_bottom]
-        self.bcs = []
-
-        problem = LinearProblem(a, L, bcs=self.bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu",
-                                                            "pc_factor_mat_solver_type": "mumps"})
         try:
-            w_h = problem.solve()
+            # projector = Projector(f, V, bcs = [])
+            p_h = problem.solve()
+            # vf = projector(-kappa_over_mu * grad(p_h) / phi)
+            # fig = plt.figure()
+            # im = plot(vf)
+            # plt.colorbar(im, format="%.2e")
         except PETSc.Error as e:  # type: ignore
             if e.ierr == 92:
                 print("The required PETSc solver/preconditioner is not available. Exiting.")
@@ -175,11 +156,11 @@ class PerfusionSolver:
             else:
                 raise e
 
-        sigma_h, u_h = w_h.split()
+        # sigma_h, u_h = w_h.split()
 
-        with io.XDMFFile(self.mesh.comm, "out_mixed_poisson/u.xdmf", "w") as file:
+        with io.XDMFFile(self.mesh.comm, "out_darcy/u.xdmf", "w") as file:
             file.write_mesh(self.mesh)
-            file.write_function(u_h)
+            file.write_function(p_h)
         
 if __name__ == '__main__':
 
@@ -188,8 +169,8 @@ if __name__ == '__main__':
 
     T = 1.5 # Final simulation time
     dt = 0.01 # Timestep size
-    L = 1.0 # Length of the domain
-    H = 2.0 # Height of the domain
+    L = 2.0 # Length of the domain
+    H = 1.0 # Height of the domain
     N = 64 # Number of mesh cells
     # N = int(argv[1]) # Number of mesh cells
     D_value = 1e-2 # Diffusion coefficient
@@ -203,4 +184,3 @@ if __name__ == '__main__':
                                     element_degree=k,
                                     write_output=write_output)
     perfusion_sim.setup()
-
